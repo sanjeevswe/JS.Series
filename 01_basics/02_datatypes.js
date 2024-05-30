662996f513a1ea67f18254e30239da834ba6b919
Sanjeev Kumar <75729980+Sanjeevswe46@users.noreply.github.com>
2024-05-30 18:43:06 +0530

    different datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//alert (3 + 3) // we are using nodejs not browser
+
+console.log(3 + 3);
+
+console.log("Sanjeev")
+
+
+//number => 9 to power 53
+//boolean = true/false
+//bigint 
+//string => ""
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);       //object
