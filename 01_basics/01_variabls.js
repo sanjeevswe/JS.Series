7df07f073c2c82095a0ac667259c8d0e7119a955
Sanjeev Kumar <75729980+Sanjeevswe46@users.noreply.github.com>
2024-05-30 18:07:27 +0530

    renamed

--- 01_basics/01_variabls.js
+++
@@ -1,8 +0,0 @@
-const accountId = 8624
-let accountEmail = "sanjeev123@gmail.com"
-var accountPassword = "54321"
-accountCity = "Jaipur"
-
-// accountId = 9  //not allowed
-
-console.log(accountId);
