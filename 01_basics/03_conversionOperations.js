963138208fb9c0c105513d3c42ce31804998d10d
Sanjeev Kumar <75729980+Sanjeevswe46@users.noreply.github.com>
2024-06-03 17:07:46 +0530

    conversion of data

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,4 +1,26 @@
-let score = 52
+let score = null
 
-console.log(typeof score);
-console.log(typeof(score));
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+// "52" => 52
+// "52asc" => NaN
+// "null" => 0
+// "true" => 1 "false" =>  0
+
+let isLoggedIn = "Sanjeev"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Sanjeev" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
