abd1f7553b2478bfe5526ae507228c34366648ae
Sanjeev Kumar <75729980+Sanjeevswe46@users.noreply.github.com>
2024-06-03 17:50:08 +0530

    study on operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -23,4 +23,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+
+//************************************Operations**********************************
+
+let valu = 3
+let negValue = -valu
+//console.log(negValue);
+
+//console.log(2**3);
+
+let str1 = "Sanjeev"
+let str2 = " Kumar"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log( "1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+//console.log((3 + 4) * 5 % 3);
+
+//console.log(+true);
+//console.log(+""); 
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+//console.log(num1);
+//console.log(num2);
+//console.log(num3);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
