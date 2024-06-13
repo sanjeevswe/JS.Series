598e5190070dd1ad7833ac785d5a531876d79c80
Sanjeev Kumar <75729980+Sanjeevswe46@users.noreply.github.com>
2024-06-13 18:01:26 +0530

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,37 @@
+const score = 100
+
+//console.log(score);
+
+const balance = new Number(1680)
+
+//console.log(balance.toFixed(2));
+
+const otherNumber = 86.6451
+
+//console.log(otherNumber.toPrecision(2));
+
+const ind = 7370008624
+
+//console.log(ind.toLocaleString());
+
+
+
+
+/**********************************************Maths*************************************** */
+
+// console.log(Math);
+// console.log(Math.abs(-2));
+// console.log(Math.round(5.62));
+// console.log(Math.ceil(6.1));
+// console.log(Math.floor(6.9));
+// console.log(Math.min(2,5,1,55,33,46));
+// console.log(Math.max(2,5,1,55,33,46));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min )
+
